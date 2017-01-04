2abfeaca0ff55a206114983f99d087dd0d6958dc
shutton1 <shutton1@stanford.edu>
2017-01-04 09:02:32 -0800

    Update sanitizer.py

--- sanitizer.py
+++ sanitizer.py
@@ -2,147 +2,122 @@ import os,sys
 import re
 import string
 
-#Sydney says hello!
 
-def getRegex_phone():
-  regular_expression = re.compile(r"\(?"  # open parenthesis
-                                  r"(\d{3})?"  # area code
-                                  r"\)?"  # close parenthesis
-                                  r"[\t\s\.-]{0,5}?"  # area code, phone separator
-                                  r"(\d{3})"  # 3 digit local
-                                  r"[\s\.-]{0,3}"  # 3 digit local, 4 digit local separator
-                                  r"(\d{4})"  # 4 digit local
-                                  , re.IGNORECASE)
-  return regular_expression
+def get_input_file():
 
-def getRegex_zip():
-  regular_expression = re.compile(r'.*(\d{5}(\-\d{4})?)$')
-  return regular_expression
+  if(len(sys.argv) != 2):
+    print 'Usage: python sanitizer_new.py <filename.pdf>'
+    return
+  pdf_input_string = sys.argv[1]
+  return pdf_input_string[:-4]
 
 
-def removePhoneAndWeb(line, tags, regex):
-  for word in line.split():
-      #remove web#
-    for tag in tags:
-      if tag in word.lower():
-        line = line.replace(word, '<WEB-ADDRESS>')
-        break
-      #end#
-
-    try:
-      result = re.search(regex_phone, word)
-      if result:
-        line = line.replace(word, '<PHONE-NUMBER>')
-    except:
-      pass
-    try:
-      result = re.search(regex_zip, word)
-      if result:
-        line = line.replace(word, '<ZIP-CODE>')
-    except:
-       pass
-  return line
+def get_txt_filename(input_string):
+  os.system("./pdftotext %s.pdf -enc UTF-8 -layout" % str(input_string))
+  txt_filename = input_string + ".txt"
+  return txt_filename
 
 
-def removePII(resumeLines, pii):
-  tags =  ['.com', '.edu', 'http', 'https', '.org', '.net', 'www.']
-  regex_phone = getRegex_phone()
-  regex_zip = getRegex_zip()
-  #removePhoneAndWeb(resumeLines)
-  i = 0 #ratchet -thomas
-  for line in resumeLines:
-    line = removePhoneAndWeb(line, tags, regex_phone, regex_zip)
-    for pairs in pii:
-      pair = pairs.split('\\')
-      line = line.replace(pair[0], '<' + pair[1] + '>')
-    resumeLines[i] = line
-    i = i + 1
-  return resumeLines
+def concatenate_string_list(resume_lines):
+  resume_str = ''
+  for line in resume_lines:
+    line = line.strip().replace('\t', ' ') + ' '
+    resume_str = resume_str + line + ','
+  return resume_str
 
 
+def call_NLP(resume_lines):
+  resume_str = concatenate_string_list(resume_lines)
+  resume_str = "\"" + resume_str + "\""
+  os.system("python analyze.py entities %s > entities.txt" % str(resume_str))
 
-def getEntities(resumeLines, entityFile):
+def get_entities(resume_lines, entity_file):
   entities = ['LOCATION', 'ORGANIZATION', 'PERSON']
   pii = []
-  for entityLine in entityFile:
-    for word in entities: #possible bug if the user has token 'LOCATION' or 'ORGANIZATION' or 'PERSON' in resume
-      if word in entityLine:
-        next_line = next(entityFile)
-        next_line = next_line.replace('"name": "', '')
-        next_line = next_line.replace('",', '')
-        next_line = next_line.strip()
-
-        if len(next_line) > 1:
-          if word != 'PERSON' or next_line[0].isupper():
-            #print word + " : " + next_line
-            pii.append(next_line + '\\' + word)
-       #break out of for loop
+  for entity_line in entity_file:
+    if 'COMMON' in entity_line:
+      next(entity_file)
+      next(entity_file)
+      next(entity_file)
+      next(entity_file)
+      next(entity_file)
+    else:
+      for word in entities:
+        if word in entity_line:
+          next_line = next(entity_file)
+          next_line = next_line.replace('"name": "', '')
+          next_line = next_line.replace('",', '')
+          next_line = next_line.strip()
+          if len(next_line) > 1:
+            if word != 'PERSON' or next_line[0].isupper():
+              print 'adding pii: ' + next_line
+              pii.append(next_line + '\\' + word)
   pii.sort(lambda x,y: cmp(len(y), len(x)))
   return pii
 
 
-def concatenateStringList(resumeLines):
-  resumeStr = ''
-  for line in resumeLines:
-    line = line.strip().replace("\t", " ") + ' '
-    resumeStr = resumeStr + line + "," #PLUS NEWLINE SEPARATOR XXX
-  return resumeStr
+def get_regex_phone():
+  regular_expression = re.compile(r"\(?"
+                                  r"(\d{3})?"
+                                  r"\)?"
+                                  r"[\t\s\.-]{0,5}?"
+                                  r"(\d{3})"
+                                  r"[\s\.-]{0,3}"
+                                  r"(\d{4})"
+                                  , re.IGNORECASE)
+  return regular_expression
 
+def get_regex_zip():
+  regular_expression = re.compile(r'.*(\d{5}(\-\d{4})?)$')
+  return regular_expression
 
-def callNLP(resumeLines):
-  resumeStr = concatenateStringList(resumeLines)
-  resumeStr = "\"" + resumeStr + "\""
-  os.system("python analyze.py entities %s > entities.txt" % str(resumeStr))
-  return 'entities.txt' #this is if we want a custom entity file TODO: remove
-  
+def remove_phone_and_web(line, tags, regex_phone, regex_zip):
+  for word in line.split():
+    for tag in tags:
+      if tag in word.lower():
+        line = line.replace(word, '<WEB-ADDRESS>')
+    try:
+      result = re.search(regex_phone, word)
+      if result:
+        line = line.replace(word, '<PHONE-NUMBER>')
+      result = re.search(regex_zip, word)
+      if result:
+        line = line.replace(word, '<ZIP>')
+    except:
+      pass
+  return line
 
+def remove_pii(resume_lines, pii):
+  tags = ['@', '.com', '.edu', 'http', 'https', '.org', '.net', 'www.']
+  regex_phone = get_regex_phone()
+  regex_zip = get_regex_zip()
+  for i,line in enumerate(resume_lines):
+    line = remove_phone_and_web(line, tags, regex_phone, regex_zip)
+    for pairs in pii:
+      pair = pairs.split('\\')
+      line = line.replace(pair[0] + ' ', '<' + pair[1] + '> ')
+      line = line.replace(' ' + pair[0], ' <' + pair[1] + '>')
+    resume_lines[i] = line
+  return resume_lines
 
-def getResumeTxtFilename(inputString):
-  os.system("./pdftotext %s.pdf -enc UTF-8 -layout" % str(inputString))
-  txtFilename = inputString + ".txt"
-  return txtFilename
-
-def getInputFile():
-  if(len(sys.argv) != 2):
-    print 'Usage: python sanitizer.py <filename.pdf>'
-    return
-  pdfInputString = sys.argv[1]
-  return pdfInputString[:-4]
 
 
 if __name__ == '__main__':
+  input_string = get_input_file() #ex. hutton_resume
 
-  '''outline of steps in this process'''
-  # 1. get desired pdf file
-  # 2. run that file through pdf2text
-  # 3. take the output of pdf2text and run that through google NLP, getting a pii list from that
-  # 4. go through the pdf2text output again, this time removing pii from pii list and saving them into line list
-  # 5. read lines from line list into a new file
-  '''end'''
+  txt_filename = get_txt_filename(input_string) #ex. hutton_resume.pdf
+  with open(txt_filename) as resume_txt_file:
+    resume_lines = resume_txt_file.readlines()
 
-    #step 1
-inputString = getInputFile()
-#inputString = 'resumehutton_pdf' #temporary
+  copy_lines = list(resume_lines)
+  call_NLP(copy_lines)
+  entity_file = open('entities.txt', 'r')
+  pii = get_entities(resume_lines, entity_file)
+  entity_file.close()
 
-    #step 2
-resumeTxtFilename = getResumeTxtFilename(inputString)
-#resumeTxtFilename =  'resumehutton_pdf.txt' #temporary
-with open(resumeTxtFilename) as resumeTxtFile:
-  resumeLines = resumeTxtFile.readlines()
+  edited_lines = remove_pii(resume_lines, pii)
 
-    #step 3
-entityFilename = callNLP(resumeLines)
-#entityFilename = 'huttonEntities.txt' #temporary
-entityFile = open(entityFilename, 'r')
-pii = getEntities(resumeLines, entityFile)
-entityFile.close()
-  
-    #step 4
-editedLines = removePII(resumeLines, pii)
-
-    #step 5
-output = open(inputString + '_sanitized.txt', 'w')
-for line in editedLines:
-  output.write(line)
-output.close()
-
+  output = open(input_string + '_sanitized.txt', 'w')
+  for line in edited_lines:
+    output.write(line)
+  output.close()
